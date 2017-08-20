65d9568948fd0744662c17e5f56ee281f1c2e36a
Shantam Vijayputra <vshantam@yahoo.com>
2017-08-20 19:11:07 +0530

    Add files via upload

---
+++ lib.py
@@ -0,0 +1,13 @@
+
+import pandas as pd
+import quandl
+import math
+import os
+import numpy as np
+from sklearn import preprocessing,cross_validation,svm
+from sklearn.linear_model import LinearRegression
+import datetime
+import matplotlib.pyplot as plt
+from matplotlib import style
+
+
